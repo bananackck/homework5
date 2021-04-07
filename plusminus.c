ce75cbe49d18255582d58454038c2e64f2688629
Bukyoung Kim <emilly03141221@gmail.com>
2021-04-07 19:04:07 +0900

    init

---
+++ plusminus.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define FLUSH while(getchar()!='\n');
+int main() {
+	int a, b;
+	char calc;
+
+	printf("Numbers? ");
+	scanf("%d %d", &a, &b);
+	printf("Calculation? ");
+	FLUSH;
+	scanf("%c", &calc);
+
+	if (calc == '+')
+		printf("Result is %d\n", a + b);
+
+	return 0;
+}
